6e59582e7c31c7ea4ffe0e4c513136d1bca5fdec
Josué López <jomalolep@gmail.com>
2024-04-11 21:34:38 -0600

    feature: search & default page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Figtree } from "next/font/google";
 import "./globals.css";
+import SearchBar from "@/components/SearchBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={figtree.className}>
+        <SearchBar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
