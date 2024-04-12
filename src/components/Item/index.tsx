8e21fb9060b4ced0edf9862ff458d8de9219dbc6
Josué López <jomalolep@gmail.com>
2024-04-12 11:27:18 -0600

    feature: item component

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import "./styles.scss";
+import formatPrice from "@/helpers/formatPrice";
+
+interface Price {
+  currency: string;
+  amount: number;
+  decimals: number;
+}
+
+interface Item {
+  id: string;
+  title: string;
+  price: Price;
+  picture: string;
+  condition: string;
+  free_shipping: boolean;
+}
+
+interface ItemProps {
+  details: Item;
+}
+
+const Item = ({ details }: ItemProps) => {
+  const { title, price, picture, free_shipping } = details;
+  const formattedPrice = formatPrice(price);
+
+  return (
+    <li className="item">
+      <Link href={`/items/`}>
+        <Image src={picture} alt={title} width={180} height={180} />
+        <div className="item-content">
+          <div className="main-info">
+            <div className="price-details">
+              <span>{formattedPrice}</span>
+              {free_shipping && (
+                <Image
+                  src="/images/ic_shipping.png"
+                  alt="imagen"
+                  width={18}
+                  height={18}
+                />
+              )}
+            </div>
+
+            <h2>{title}</h2>
+          </div>
+
+          <span className="city-info">Ciudad</span>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default Item;
