860a2452871fe97cfcb70d8d8407d79000e45008
Josué López <jomalolep@gmail.com>
2024-04-15 21:45:34 -0600

    fix: images size and responsive

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -65,7 +65,6 @@ export default async function ProductDetailPage({
               priority
               style={{
                 width: "100%",
-                height: "auto",
               }}
               sizes="100vw"
             />
